bfdbaadb96569e4dade63335a8f604d452fb7b63
Sakshi Shreya <shreyasakshi96@gmail.com>
2024-04-12 00:21:53 +0530

    Try surge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
-    <div>Hi</div>
+    <RouterProvider router={router} />
   );
 }
 
