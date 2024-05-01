300bf95ca2f299bf3802f5e94fd1422b15411f17
Sakshi Shreya <shreyasakshi96@gmail.com>
2024-05-01 22:40:30 +0530

    Add tailwind, nextui and basic app structure

---
+++ src/planner/common/navbar.tsx
@@ -0,0 +1,39 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link } from "@nextui-org/react";
+import { useState } from "react";
+
+const menuItems = [
+  {
+    label: "Dashboard",
+    href: ""
+  },
+  {
+    label: "Ingredients",
+    href: "/ingredients"
+  }
+];
+
+type Props = {};
+
+export default function NavBar({}: Props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden" />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">PLANNER</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map(item => (
+          <NavbarMenuItem key={item.label}>
+            <Link color={"foreground"} className="w-full" href={`/planner${item.href}`} size="lg">
+              {item.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
