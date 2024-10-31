3e669f1acd121df26535c8166c90f87c35400d24
Sakshi Shreya <shreyasakshi96@gmail.com>
2024-11-01 02:54:42 +0530

    42: Add all apis in rtk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { NextUIProvider } from "@nextui-org/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Toast from "./common/toast";
@@ -9,25 +8,22 @@ import Recipes from "./planner/recipes";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-const queryClient = new QueryClient();
 function App() {
   return (
     <NextUIProvider>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<div>Hello world!</div>} />
-              <Route path="planner" element={<Planner />}>
-                <Route index element={<div>Planner</div>} />
-                <Route path="ingredients" element={<Ingredients />} />
-                <Route path="recipes" element={<Recipes />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-          <Toast />
-        </Provider>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>Hello world!</div>} />
+            <Route path="planner" element={<Planner />}>
+              <Route index element={<div>Planner</div>} />
+              <Route path="ingredients" element={<Ingredients />} />
+              <Route path="recipes" element={<Recipes />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toast />
+      </Provider>
     </NextUIProvider>
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
-import toastSliceReducer from './common/toast/slice'
-import { useDispatch, useSelector } from 'react-redux'
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import toastSliceReducer from "./common/toast/slice";
+import { ingredientsApi } from "./planner/ingredients/api";
 
 export const store = configureStore({
   reducer: {
     toast: toastSliceReducer,
+    ingredientsApi: ingredientsApi.reducer
   },
-})
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(ingredientsApi.middleware)
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
