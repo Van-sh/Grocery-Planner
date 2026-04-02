bff3e3efff43d9ade5a8e77c7d31a2bf82343b68
Van-sh <72124535+Van-sh@users.noreply.github.com>
2026-04-03 01:01:11 +0530

    Fix Incorrect rebase

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -13,12 +13,19 @@ import {
   NavbarMenuToggle,
 } from "@heroui/react";
 import { createSelector } from "@reduxjs/toolkit";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector, type RootState } from "../store";
+import { useGetCurrentUserQuery } from "../user/api";
 import { isLoggedIn } from "./auth/helper";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
-import { closeLoginModal, closeSignupModal, openLoginModal, openSignupModal } from "./auth/slice";
+import {
+  addUserDetails,
+  closeLoginModal,
+  closeSignupModal,
+  openLoginModal,
+  openSignupModal,
+} from "./auth/slice";
 import UserMenu from "./auth/userMenu";
 
 const menuItems = [
